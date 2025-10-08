054b5f90038929019088835fb384508e50b9db60
Your Name <you@example.com>
2025-10-08 19:57:01 +0200

    item test

--- scripts/Game/Systems/InventorySystem/Item.cs
+++ scripts/Game/Systems/InventorySystem/Item.cs
@@ -7,7 +7,7 @@ namespace TnT.EduGame.Inventory
     /// Represents an in-game Item, based on an ItemData Resource.
     /// </summary>
     [GlobalClass]
-    public partial class Item : Node
+    public partial class Item : Node2D
     {
         public Action<Item> OnItemUsed;
 
@@ -17,11 +17,8 @@ namespace TnT.EduGame.Inventory
         string _id = Guid.NewGuid().ToString();
         public string Id => _id;
 
-        [Export]
         public bool IsPersistent { get => _staticData.IsPersistent; private set => _staticData.IsPersistent = value; }
-        [Export]
         public string ItemId { get => _staticData.Id; private set => _staticData.Id = value; }
-        [Export]
         public Texture2D Icon { get => _staticData.Icon; private set => _staticData.Icon = value; }
 
         public ItemData Data => _staticData;
