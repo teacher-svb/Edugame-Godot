180843dd939f6adfce0fde77b7b9b98eb9955238
syndias <sam@vanbattel.be>
2026-04-11 21:55:02 +0200

    occlusion shader fade-in/out

--- scripts/GridMapOcclusionFader.cs
+++ scripts/GridMapOcclusionFader.cs
@@ -6,8 +6,12 @@ public partial class GridMapOcclusionFader : GridMap
     [Export] public Shader OcclusionShader { get; set; }
     [Export] public Node3D Player { get; set; }
     [Export] public Camera3D Camera { get; set; }
+    [Export] public RayCast3D PlayerRayCast { get; set; }
+    [Export] public float FadeSpeed { get; set; } = 2.0f; // units per second
+    [Export] public float MaxRadius { get; set; } = 2.0f; // match hole_radius in shader
 
     private List<ShaderMaterial> _allMaterials = new();
+    [Export] private float _currentRadius = 0.0f;
 
     public override void _Ready()
     {
@@ -34,8 +38,18 @@ public partial class GridMapOcclusionFader : GridMap
     public override void _Process(double delta)
     {
         RenderingServer.GlobalShaderParameterSet("player_pos", Player.GlobalPosition);
+
+        var camPos = Camera.GlobalPosition;
         foreach (var mat in _allMaterials)
-            mat.SetShaderParameter("camera_pos", Camera.GlobalPosition);
+            mat.SetShaderParameter("camera_pos", camPos);
+
+        PlayerRayCast.TargetPosition = PlayerRayCast.ToLocal(camPos);
+
+        float targetRadius = PlayerRayCast.IsColliding() ? MaxRadius : 0.0f;
+        _currentRadius = Mathf.MoveToward(_currentRadius, targetRadius, (float)delta * FadeSpeed * MaxRadius);
+
+        foreach (var mat in _allMaterials)
+            mat.SetShaderParameter("hole_radius", _currentRadius);
     }
 
     private Mesh ApplyMaterialOverride(Mesh original)
