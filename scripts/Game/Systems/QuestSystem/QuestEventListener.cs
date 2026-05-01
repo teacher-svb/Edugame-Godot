a2d2c481b2db4710d7be11e0202e03f6b9f3ccd3
syndias <sam@vanbattel.be>
2026-05-01 20:38:26 +0200

    simplifying the questeventlistener, merging with the reactor

--- scripts/Game/Systems/QuestSystem/QuestEventListener.cs
+++ scripts/Game/Systems/QuestSystem/QuestEventListener.cs
@@ -1,5 +1,4 @@
-
-using System;
+using System.Linq;
 using Godot;
 using TnT.Systems.EventSystem;
 
@@ -8,14 +7,19 @@ namespace TnT.EduGame.QuestSystem
     [GlobalClass]
     public partial class QuestEventListener : EventListener
     {
-        [Export]
-        bool _checkObjectiveId;
-        [Export]
-        string _objectiveId;
-        [Export]
-        bool _checkState;
-        [Export]
-        QuestState _state;
+        [Export] bool _checkObjectiveId;
+        [Export] string _objectiveId;
+        [Export] bool _checkState;
+        [Export] QuestState _state;
+
+        private QuestReaction[] _reactions = [];
+
+        public override void _Ready()
+        {
+            base._Ready();
+            _reactions = GetChildren().OfType<QuestReaction>().ToArray();
+        }
+
         public override void Raise(params Variant[] values)
         {
             foreach (var value in values)
@@ -24,12 +28,16 @@ namespace TnT.EduGame.QuestSystem
                 {
                     if (_checkObjectiveId && o.ObjectiveId != _objectiveId) return;
                     if (_checkState && o.State != _state) return;
-
-                    GD.Print($"Quest {o.ObjectiveId} state changed to {o.State}");
-
                     base.Raise(o);
+                    ExecuteReactions();
                 }
             }
+        }
+
+        private async void ExecuteReactions()
+        {
+            foreach (var reaction in _reactions)
+                await reaction.Execute();
         }
     }
 }
